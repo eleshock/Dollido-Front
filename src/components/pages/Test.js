3185c3c45e1e8054f9776ce0aeb2f0e87d131ab2
yunchanpark <yongp99@naver.com>
2022-07-11 05:26:39 +0900

    typescript setting

--- src/components/pages/Test.js
+++ src/components/pages/Test.js
@@ -1,4 +1,15 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const Test = () => {
+    const callApi = async () => {
+        axios.get("/api/home").then((res) => console.log(res.data.test));
+    };
+
+    useEffect(() => {
+        callApi();
+    }, []);
+
     return (
         <div>
             <h1>Test</h1>
