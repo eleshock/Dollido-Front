7b4f3f9a25a97521d10c036b3767181800e2622c
yoonwoo_labtop <mryoonwoo@naver.com>
2022-07-13 03:45:35 +0900

    내 카메라 in game 페이지에 연결함

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Main from "./components/pages/Main";
-import Test from "./components/pages/Test";
-import Room from "./components/pages/Room";
-import Test from "./components/pages/Test";
+import Test from "./components/pages/MyVideo";
+// import Room from "./components/pages/Room";
 import Giftest from "./components/pages/Giftest";
 import InGame from "./components/pages/InGame"
 import {BrowserRouter, Routes, Route} from "react-router-dom";
@@ -12,7 +11,7 @@ function App()  {
             <Routes>
                 <Route path="/" element={<Main/>}></Route>
                 <Route path="/test" element={<Test/>}></Route>
-                <Route path="/room" element={<Room/>}></Route>
+                {/* <Route path="/room" element={<Room/>}></Route> */}
                 <Route path="/gif_front" element={<Giftest/>}></Route>
                 <Route path="/play" element={<InGame/>}></Route>
             </Routes>
--- src/components/pages/Room.js
+++ src/components/pages/Room.js
@@ -1,40 +1,40 @@
-import axios from "axios";
-import { useEffect, useState, Link } from "react";
-import io from "socket.io-client"
-const socket = io("http://13.209.66.46:5000/");
+// import axios from "axios";
+// import { useEffect, useState, Link } from "react";
+// import io from "socket.io-client"
+// const socket = io("http://13.209.66.46:5000/");
 
-function Room() {
-    let [roomName, setRoomName] = useState("");
+// function Room() {
+//     let [roomName, setRoomName] = useState("");
 
-    useEffect(() => {
-        socket.on("welcome", (newbieID, room) => {
-            console.log(newbieID, room);
-        });
-    }, [socket]);
+//     useEffect(() => {
+//         socket.on("welcome", (newbieID, room) => {
+//             console.log(newbieID, room);
+//         });
+//     }, [socket]);
 
-    function joinRoom() {
-        console.log("hi")
-        socket.emit("join_room", roomName, socket.id);
-    }
+//     function joinRoom() {
+//         console.log("hi")
+//         socket.emit("join_room", roomName, socket.id);
+//     }
     
-    function hi() { 
-        console.log("hi")
-        axios.get("http://13.209.66.46:5000/api/test");
-    }
+//     function hi() { 
+//         console.log("hi")
+//         axios.get("http://13.209.66.46:5000/api/test");
+//     }
 
-    return (
-        <div>
-            < input type = "text"
-            onChange = {
-                (event) => setRoomName({
-                    enteredValue: event.target.value
-                })
-            }
-            />
-            <button onClick={joinRoom}>방만들기zzzzzzzzz</button>
-            <button onClick={hi}>zz</button>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             < input type = "text"
+//             onChange = {
+//                 (event) => setRoomName({
+//                     enteredValue: event.target.value
+//                 })
+//             }
+//             />
+//             <button onClick={joinRoom}>방만들기zzzzzzzzz</button>
+//             <button onClick={hi}>zz</button>
+//         </div>
+//     )
+// }
 
-export default Room;
+// export default Room;
