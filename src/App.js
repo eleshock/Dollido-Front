0016a7bd33fe137cfca6210eaba23e669ebd3279
Donghee <hdl93@naver.com>
2022-07-19 04:35:19 +0900

    universe page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import InGame from "./components/pages/InGame";
 import Lobby from "./components/pages/Lobby";
 import Test from "./components/pages/Test";
 import Images from "./components/pages/Images";
+import Universe from "./components/pages/Universe";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App()  {
@@ -20,6 +21,7 @@ function App()  {
                 <Route path="/room/:roomID" element={<Room/>} />
                 <Route path="/test" element={<Test/>}/>
                 <Route path="/images" element={<Images/>}/>
+                <Route path="/univ" element={<Universe/>}/>
             </Routes>
         </BrowserRouter>
     );
