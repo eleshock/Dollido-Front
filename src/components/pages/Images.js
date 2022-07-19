d2e0057867f48e5aeb11248871d161c9842eaafd
Donghee <hdl93@naver.com>
2022-07-20 02:16:04 +0900

    dongbro hey~~

--- src/components/pages/Images.js
+++ src/components/pages/Images.js
@@ -4,38 +4,38 @@ import { ServerName } from "../../serverName";
 
 import './Images.css'
 
-async function postImage({image, description}) {
+async function postImage({image}) {
   const formData = new FormData();
-  formData.append("image", image, encodeURI(image));
-  formData.append("description", description)
-
-
-  const result = await axios.post(`${ServerName}/api/gifs/images`, formData, { headers: {'Content-Type': 'multipart/form-data'}}).then((req) => console.log(req.data.imagePath))
+  formData.append("image", image);
+    console.log(image)
+    console.log(formData)
+const result = await axios.post(`${ServerName}/api/gifs/images`, formData, { headers: {'Content-Type': 'multipart/form-data; charset=utf-8', "ACCEPT": "*/*"}})
+  console.log(result)
+  return result.data
 }
 
 
 function Images() {
 
   const [file, setFile] = useState("")
-  const [description, setDescription] = useState("")
   const [images, setImages] = useState([])
 
   const submit = async event => {
     event.preventDefault()
-    const result = await postImage({image: file, description})
+    const result = await postImage({image: file})
     setImages([result.image, ...images])
   }
 
   const fileSelected = event => {
     const file = event.target.files[0]
-		setFile(file)
+    console.log(file)
+    setFile(file)
 	}
 
   return (
     <div className="App">
-      <form onSubmit={submit}>
+      <form onSubmit={submit} enctype = "multipart/form-data" accept-charset="UTF-8">
         <input onChange={fileSelected} type="file" accept="image/*"></input>
-        <input value={description} onChange={e => setDescription(e.target.value)} type="text" ></input>
         <button type="submit">Submit</button>
       </form>
 
