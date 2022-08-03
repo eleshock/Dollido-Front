5c5e78897cf1520daca4548c4d4c3348c72a46be
Donghee <hdl93@naver.com>
2022-08-03 15:15:32 +0900

    profile 수정

--- src/components/pages/myPage/Profile.js
+++ src/components/pages/myPage/Profile.js
@@ -1,57 +1,13 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import Moai from "../../../images/Moai2.png";
 import Kaonish from "../../../images/Kaonish.png";
 import Monarisa from "../../../images/Monarisa.png";
 import KoreanMask from "../../../images/KoreanMask.png";
 
+import { useSelector } from "react-redux";
 
-export default function profile() {
 
-    return (
-        <div style = {{display: "flex", justifyContent: "center"}}>
-            <Profile>
-                <TierImage>
-                <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={Moai} />
-                </TierImage>
-                <div style = {{display: "flex", flexDirection: "column"}}>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
-                        <ProfileAlign>
-                            <ProfileTitle>닉네임</ProfileTitle>
-                            <ProfileBorder></ProfileBorder>
-                            <ProfileContent>나웃는거본사람</ProfileContent>
-                        </ProfileAlign>
-                        <ProfileAlign>
-                            <ProfileTitle>랭킹</ProfileTitle>
-                            <ProfileBorder></ProfileBorder>
-                            <ProfileContent>1 위</ProfileContent>
-                        </ProfileAlign>
-                        <ProfileAlign>
-                            <ProfileTitle>전적</ProfileTitle>
-                            <ProfileBorder></ProfileBorder>
-                            <ProfileContent>103</ProfileContent>
-                            <ProfileContent style = {{fontSize:"1.4rem", padding: "3px 0 0 0"}}>승</ProfileContent>
-                            <ProfileContent>/</ProfileContent>
-                            <ProfileContent>10</ProfileContent>
-                            <ProfileContent style = {{fontSize:"1.4rem", padding: "3px 0 0 0"}}>패</ProfileContent>
-                        </ProfileAlign>
-                        <ProfileAlign>
-                            <ProfileTitle>승률</ProfileTitle>
-                            <ProfileBorder></ProfileBorder>
-                            <ProfileContent>90 %</ProfileContent>
-                        </ProfileAlign>
-                        <ProfileAlign>
-                            <ProfileTitle>티어</ProfileTitle>
-                            <ProfileBorder></ProfileBorder>
-                            <ProfileContent>모아이</ProfileContent>
-                        </ProfileAlign>
-                    </div>
-                </div>
-            </Profile>
-        </div>
-    );
-}
-
-const Profile = styled.div`
+const Container = styled.div`
     display: flex;
     flex-direction: row;
     width: 590px;
@@ -63,12 +19,13 @@ const Profile = styled.div`
     justify-content: center;
     align-items: center;
     background-color: rgba( 255, 255, 255, 0.3 );
+
     box-shadow: 5px 5px rgba( 255, 255, 255, 0.2 );
     &:hover {
         background-color: #00ffff79; // 다이아
-        ${'' /* background-color: #ffe14079; // 골드 
-        background-color: #C0C0C0A9; // 실버 */}
-        ${'' /* background-color: #C3672999; // 브론즈 */}
+        // background-color: #ffe14079; // 골드
+        // background-color: #C0C0C0A9; // 실버
+        // background-color: #C3672999; // 브론즈
         transition: background-color 1.5s;
     }
 `
@@ -116,3 +73,63 @@ const ProfileBorder = styled.div`
     border-left: 2px solid white;
     height: 1.3rem;
 `
+
+export default function Profile() {
+    const tier = useSelector((state) => state.member.tier);
+    const ranking = useSelector((state) => state.member.ranking);
+    const winRate = useSelector((state) => state.member.win_rate);
+    const win = useSelector((state) => state.member.win);
+    const lose = useSelector((state) => state.member.lose);
+    const nickname = useSelector((state) => state.member.member.user_nick);
+
+    let image;
+
+    if (tier === "모나리자") image = Monarisa
+    else if (tier === "모아이") image = Moai
+    else if (tier === "가오나시") image = Kaonish
+    else image = KoreanMask
+
+
+    return (
+        <div style = {{display: "flex", justifyContent: "center"}}>
+            <Container>
+                <TierImage>
+                <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={image} />
+                </TierImage>
+                <div style = {{display: "flex", flexDirection: "column"}}>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <ProfileAlign>
+                            <ProfileTitle>닉네임</ProfileTitle>
+                            <ProfileBorder></ProfileBorder>
+                            <ProfileContent>{nickname}</ProfileContent>
+                        </ProfileAlign>
+                        <ProfileAlign>
+                            <ProfileTitle>랭킹</ProfileTitle>
+                            <ProfileBorder></ProfileBorder>
+                            <ProfileContent>{ranking}</ProfileContent>
+                        </ProfileAlign>
+                        <ProfileAlign>
+                            <ProfileTitle>전적</ProfileTitle>
+                            <ProfileBorder></ProfileBorder>
+                            <ProfileContent>{win}</ProfileContent>
+                            <ProfileContent style = {{fontSize:"1.4rem", padding: "3px 0 0 0"}}>승</ProfileContent>
+                            <ProfileContent>/</ProfileContent>
+                            <ProfileContent>{lose}</ProfileContent>
+                            <ProfileContent style = {{fontSize:"1.4rem", padding: "3px 0 0 0"}}>패</ProfileContent>
+                        </ProfileAlign>
+                        <ProfileAlign>
+                            <ProfileTitle>승률</ProfileTitle>
+                            <ProfileBorder></ProfileBorder>
+                            <ProfileContent>{winRate} %</ProfileContent>
+                        </ProfileAlign>
+                        <ProfileAlign>
+                            <ProfileTitle>티어</ProfileTitle>
+                            <ProfileBorder></ProfileBorder>
+                            <ProfileContent>{tier}</ProfileContent>
+                        </ProfileAlign>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+}
